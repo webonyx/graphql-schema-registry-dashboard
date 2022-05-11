f2664fd4a9d30b6aae5718b29d49a7ae5ebe0753
Viet Pham <viet@webonyx.com>
2022-05-11 19:49:19 +0700

    yarn

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import TopMenu from './TopMenu';
 import TabPanel from './TabPanel';
